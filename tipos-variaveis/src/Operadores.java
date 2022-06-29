57a68fa0c64c39b96798f2bd471598dbdf0bbf0b
Maurício Mendes <mauricio.mend3s@gmail.com>
2022-06-29 16:37:20 -0300

    testing operator

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,29 @@
+import org.omg.Messaging.SyncScopeHelper;
+
+public class Operadores {
+    public static void main(String[] args) {
+    
+        String nomeCompleto = "Linguagem" + "JAVA";
+        System.out.println(nomeCompleto);
+
+        String concatenacao = "?";
+
+        concatenacao = 1+1+1+"1";
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+"1";
+        System.out.println(concatenacao);
+        
+        concatenacao = "1"+1+1+"1";
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+(1+1+1);
+        System.out.println(concatenacao);
+    }
+    
+    
+    
+}
