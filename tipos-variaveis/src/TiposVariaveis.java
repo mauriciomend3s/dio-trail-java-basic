12620efd193ecc022ef0d7fdd8f09f7625fe065b
Maurício Mendes <mauricio.mend3s@gmail.com>
2022-06-29 16:16:57 -0300

    testing variables

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,18 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        short salarioMinimo = 2500;
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+
+        short numeroCurto2 = (short)numeroNormal;
+
+        int numero = 5;
+        numero = 10;
+
+        final double VALOR_DE_PI = 3.14;
+
+        System.out.println(numero);
+        System.out.println(VALOR_DE_PI);
+
     }
 }
