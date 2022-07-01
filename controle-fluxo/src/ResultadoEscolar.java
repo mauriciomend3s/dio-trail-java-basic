7e98c7ba3350bfed3f662b75665de88161091fd1
Maurício Mendes <mauricio.mend3s@gmail.com>
2022-07-01 09:44:43 -0300

    operator ternary

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -1,3 +1,5 @@
+import org.omg.Messaging.SyncScopeHelper;
+
 public class ResultadoEscolar {
     public static void main(String[] args) {
 
@@ -5,10 +7,14 @@ public class ResultadoEscolar {
 
         if (nota >= 7) {
             System.out.println("Aprovado");
-        }else{
+        } else {
             System.out.println("Reprovado");
         }
-        
+
+        int nota2 = 5;
+        String resultado = nota2 >= 6 ? "Aprovado" : nota2 < 6 && nota2 >= 4 ? "Recuperação" : "Reprovado";
+        System.out.println(resultado);
+
     }
 
 }
