4e1b70701be20aa65aa89f0f053eb45c7b20379f
Maurício Mendes <mauricio.mend3s@gmail.com>
2022-07-01 10:44:17 -0300

    count for

---
+++ controle-fluxo/src/ExemploFor.java
@@ -0,0 +1,10 @@
+public class ExemploFor {
+    public static void main(String[] args) {
+        int carneiras = 0;
+        for(int i = 1; i<=20; i++){
+            carneiras = i;
+
+        }
+        System.out.println("Contei "+carneiras+" carneiras!");
+    }
+}
