5fff5085fccbde189cfb40b3fc557894986e5111
Maurício Mendes <mauricio.mend3s@gmail.com>
2022-07-01 21:10:16 -0300

    do while pratics

--- controle-fluxo/src/ExemplodoWhile.java
+++ controle-fluxo/src/ExemplodoWhile.java
@@ -2,21 +2,25 @@ import java.util.Random;
 
 public class ExemplodoWhile {
     public static void main(String[] args) {
+
+        boolean atendeu = false;
+        
         System.out.println("Discando...");
-
         do {
-            // excutando repetidas vezes até alguém atender
-            System.out.println("Telefone tocando");
 
+            
+            System.out.println("Telefone tocando");
+            
+            
         } while (tocando());
 
-        System.out.println("Alô !!!");
+        System.out.println("Alô!!!");
+
     }
 
-    private static boolean tocando() {
-        boolean atendeu = new Random().nextInt(3) == 1;
-        System.out.println("Atendeu? " + atendeu);
-        // negando o ato de continuar tocando
-        return !atendeu;
+    private static boolean tocando(){
+    boolean atendeu = new Random().nextInt(3)==1;
+    System.out.println("Atendeu?" + atendeu);
+    return !atendeu;
     }
 }
