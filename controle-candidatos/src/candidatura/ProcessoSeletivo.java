d6308ba1a36df7d1f65428d8ed095d3225b9418e
Maurício Mendes <mauricio.mend3s@gmail.com>
2022-07-05 10:38:11 -0300

    implementing analysis

--- controle-candidatos/src/candidatura/ProcessoSeletivo.java
+++ controle-candidatos/src/candidatura/ProcessoSeletivo.java
@@ -1,22 +1,43 @@
 package candidatura;
+import java.util.concurrent.ThreadLocalRandom;
 
-public class ProcessoSeletivo{
-public static void main(String[] args) {
-    System.out.println("Processo seletivo");
+public class ProcessoSeletivo {
+    public static void main(String[] args) {
+        System.out.println("Processo seletivo");
+        /*
+         * analisarCandidato(1900.0);
+         * analisarCandidato(2200.0);
+         * analisarCandidato(2000.0);
+         */
 
-    analisarCandidato(1900.0);
-    analisarCandidato(2200.0);
-    analisarCandidato(2000.0);
-}
-static  void analisarCandidato(double salarioPretendido){
-double salarioBase = 2000.0;
-if(salarioBase > salarioPretendido){
-    System.out.println("LIGAR PARA O CANDIDATO!");
-}else if(salarioBase == salarioPretendido){
-    System.out.println("LIGAR PARA O CANDIDATO COM CONTRA PROPOSTA");
-}else{
-    System.out.println("AGUARDANDO O RESULTADO DOS DEMAIS CANDIDATOS");
-}
-}
+    }
+
+    static void selecaoCandidatos() {
+        String[] candidatos = { "FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO", "MONICA", "FABRICIO", "MIRELA",
+                "DANIELA", "JORGE" };
+        int candidatosSelecionados = 0;
+        int candidatoAtual = 0;//esse é o indice do array ou seja ele é o responsavel por incrementar e passar para o prox candidate
+        double salarioBase = 2000.0;
+       
+        while (candidatosSelecionados < 5) {
+        String candidato = candidatos[candidatoAtual];
+double salarioPretendido = 
+        }
+    }
+
+    static void analisarCandidato(double salarioPretendido) {
+        double salarioBase = 2000.0;
+        if (salarioBase > salarioPretendido) {
+            System.out.println("LIGAR PARA O CANDIDATO!");
+        } else if (salarioBase == salarioPretendido) {
+            System.out.println("LIGAR PARA O CANDIDATO COM CONTRA PROPOSTA");
+        } else {
+            System.out.println("AGUARDANDO O RESULTADO DOS DEMAIS CANDIDATOS");
+        }
+    }
+
+    static double valorPretendido(){
+        return ThreadLocalRandom.current().nextDouble(1800,2200);
+    }
 
 }
